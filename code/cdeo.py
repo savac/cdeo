adfecbb21019084c8884c1523ac3791ab9f4be6b
rndmbx@gmail.com <rndmbx@gmail.com>
2016-02-20 15:05:49 +0000

    add cross val function

--- code/cdeo.py
+++ code/cdeo.py
@@ -5,7 +5,9 @@ import numpy as np
 from datetime import date
 import sys
 import os
+import shutil
 import re
+import copy
 import EntityExtraction
 import TimestampExtraction
 import EventEntityLinking
@@ -13,6 +15,8 @@ import EventTIMEX3Linking
 import Ordering
 import nltk.stem.porter as porter
 import cdeo_config
+from evaluation_tool import evaluation_all_function
+reload(evaluation_all_function)
 reload (cdeo_config)
 reload(cat_parser)
 reload(utils)
@@ -24,6 +28,9 @@ reload(Ordering)
 
 # init stemmer
 stemmer = porter.PorterStemmer()
+
+# 
+originalTargetEntityList = []
 
 def startUWTimeServer():
     h = subprocess.Popen(['java','-jar',cdeo_config.getConfig("uwtime_loc")])
@@ -150,86 +157,80 @@ def evaluateEventTIMEX3Linking(timeline, corpus, collection):
     print len(goldList), len(totalList)
     sys.stdout.write(('%-30s\t%3.9f\t%3.9f\t%3.9f\n' %('Total', recall, precision, f1) )) 
 
-def build_timeline(test_corpus_list, model_params):
+def build_timeline(corpus, collection, targetEntityList, model_params):
     '''Read in, predict and evaluate the test corpus'''
-    # TBD: refactor
-    res = []
-    for corpus in test_corpus_list:
-        (collection, targetEntityList) = getCollection(corpus)
-        resDir = '../data/evaluation/corpus_%s/results/' %(corpus)
-        timeline = dict()
-        print 'Predicting timeline ...'
-        # We create a dictionary to hold all the extracted linked events, entities and timestamps:
-        # Keys are target entities.
-        # Values are lists of tuples (doc_id, event_m_id, timex3_m_id, date)
-        for doc in collection:
-            if model_params.link_model == 'perceptron':
-                dictEventEntity = EventEntityLinking.linkEventEntityML(model_params.clfEntity, doc, targetEntityList, model_params.entity_syntactic_features)
-            elif model_params.link_model == 'structured_perceptron':
-                dictEventEntity = EventEntityLinking.linkEventEntitySP(model_params.w_ee, doc, targetEntityList, model_params.entity_syntactic_features)
-            for targetEntity in targetEntityList:
-                # legacy role based
-                #listEventEntity = EventEntityLinking.linkEventEntityRuleBased(doc, targetEntity) # returns list of event m_id's
-                #listEventTIMEX3 = EventTIMEX3Linking.linkEventTIMEX3(doc, listEventEntity)
+    resDir = '../data/evaluation/corpus_%s/results/' %(corpus)
+    timeline = dict()
+    print 'Predicting timeline ...'
+    # We create a dictionary to hold all the extracted linked events, entities and timestamps:
+    # Keys are target entities.
+    # Values are lists of tuples (doc_id, event_m_id, timex3_m_id, date)
+    for doc in collection:
+        if model_params.link_model == 'perceptron':
+            dictEventEntity = EventEntityLinking.linkEventEntityML(model_params.clfEntity, doc, targetEntityList, model_params.entity_syntactic_features)
+        elif model_params.link_model == 'structured_perceptron':
+            dictEventEntity = EventEntityLinking.linkEventEntitySP(model_params.w_ee, doc, targetEntityList, model_params.entity_syntactic_features)
+        for targetEntity in targetEntityList:
+            # legacy role based
+            #listEventEntity = EventEntityLinking.linkEventEntityRuleBased(doc, targetEntity) # returns list of event m_id's
+            #listEventTIMEX3 = EventTIMEX3Linking.linkEventTIMEX3(doc, listEventEntity)
 
-                listEventEntity = dictEventEntity[targetEntity]
-                
-                if model_params.link_model == 'perceptron':
-                    listEventTIMEX3 = EventTIMEX3Linking.linkEventTIMEX3ML(model_params.clfTIMEX3, doc, listEventEntity, model_params.timex_syntactic_features)
-                elif model_params.link_model == 'structured_perceptron':
-                    listEventTIMEX3 = EventTIMEX3Linking.linkEventTIMEX3SP(model_params.w, doc, listEventEntity, model_params.timex_syntactic_features)
-                
-                if not timeline.has_key(targetEntity):
-                    timeline[targetEntity] = list()
-                for event_time in listEventTIMEX3:
-                    (event_m_id, timex3_m_id) = event_time
-                    this_date = utils.str2date(utils.getTIMEX3Stamp(doc, timex3_m_id))
-
-                    event = utils.getEvent(doc, event_m_id)
-                    event_t_id = event.get_token_anchor()[0].t_id
-                    event_sentence = utils.getToken(doc, event_t_id).sentence
-                    
-                    str_event = []
-                    for a in event.get_token_anchor():
-                        str_event.append(doc.token[a.t_id-1].get_valueOf_()) # TBD: better to look for tokens searching by t_id.
-                    event_stem = stemmer.stem(str_event[0])
-                    str_event = '_'.join(str_event)
-                    str_event = str_event.lower()
-                    
-                    order = 1
-                    eventClusterId = 0
-                    lst = [doc.get_doc_id(), event_m_id, timex3_m_id, this_date, order, event_sentence, str_event, event_stem, eventClusterId] # assign an order number of 1 for now
-                    timeline[targetEntity].append(lst)
-        # Given the dictionary from above we order the tuples as sepcified by the CDEO task
-        timeline = Ordering.order(collection, timeline)
-        
-        # printing and saving to file the predicted timelines
-        for targetEntity in timeline.keys():
-            fname = targetEntity.lower()
-            fname = fname.replace('&', '_and_')
-            fname = resDir + re.sub('[^0-9a-z]+', '_', fname) + '.txt'
-            f = open(fname, 'w')
-            f.write(targetEntity)    
+            listEventEntity = dictEventEntity[targetEntity]
             
-            last_eventClusterId = -1
-            for lst in timeline[targetEntity]:
-                [doc_id, event_m_id, timex3_m_id, this_date, order, nsentence, str_event, this_stem, eventClusterId] = lst
+            if model_params.link_model == 'perceptron':
+                listEventTIMEX3 = EventTIMEX3Linking.linkEventTIMEX3ML(model_params.clfTIMEX3, doc, listEventEntity, model_params.timex_syntactic_features)
+            elif model_params.link_model == 'structured_perceptron':
+                listEventTIMEX3 = EventTIMEX3Linking.linkEventTIMEX3SP(model_params.w, doc, listEventEntity, model_params.timex_syntactic_features)
+            
+            if not timeline.has_key(targetEntity):
+                timeline[targetEntity] = list()
+            for event_time in listEventTIMEX3:
+                (event_m_id, timex3_m_id) = event_time
+                this_date = utils.str2date(utils.getTIMEX3Stamp(doc, timex3_m_id))
 
-                doc = utils.getDoc(collection, doc_id)
+                event = utils.getEvent(doc, event_m_id)
+                event_t_id = event.get_token_anchor()[0].t_id
+                event_sentence = utils.getToken(doc, event_t_id).sentence
                 
-                date_str = utils.getTIMEX3Stamp(doc, timex3_m_id)
+                str_event = []
+                for a in event.get_token_anchor():
+                    str_event.append(doc.token[a.t_id-1].get_valueOf_()) # TBD: better to look for tokens searching by t_id.
+                event_stem = stemmer.stem(str_event[0])
+                str_event = '_'.join(str_event)
+                str_event = str_event.lower()
+                
+                order = 1
+                eventClusterId = 0
+                lst = [doc.get_doc_id(), event_m_id, timex3_m_id, this_date, order, event_sentence, str_event, event_stem, eventClusterId] # assign an order number of 1 for now
+                timeline[targetEntity].append(lst)
+    # Given the dictionary from above we order the tuples as sepcified by the CDEO task
+    timeline = Ordering.order(collection, timeline)
+        
+    # printing and saving to file the predicted timelines
+    for targetEntity in timeline.keys():
+        fname = targetEntity.lower()
+        fname = fname.replace('&', '_and_')
+        fname = resDir + re.sub('[^0-9a-z]+', '_', fname) + '.txt'
+        f = open(fname, 'w')
+        f.write(targetEntity)    
+        
+        last_eventClusterId = -1
+        for lst in timeline[targetEntity]:
+            [doc_id, event_m_id, timex3_m_id, this_date, order, nsentence, str_event, this_stem, eventClusterId] = lst
 
-                # docid-sentence, event, timex3, timex3 text
-                if eventClusterId == last_eventClusterId: # TBD: this is flaky as we only check events adjacent in timeline
-                    f.write('\t' + str(doc.doc_id) + '-' + str(nsentence)+ '-' + str_event) #+  "\t('" + str_timex + "')" 
-                else:
-                    f.write('\n' + str(order) + '\t' + date_str + '\t' + str(doc_id) + '-' + str(nsentence)+ '-' + str_event) #+  "\t('" + str_timex + "')" 
-                last_eventClusterId = eventClusterId
-                last_stem = this_stem
-            f.close()
-        res += [(timeline, corpus, collection)]
-    return res
+            doc = utils.getDoc(collection, doc_id)
+            
+            date_str = utils.getTIMEX3Stamp(doc, timex3_m_id)
 
+            # docid-sentence, event, timex3, timex3 text
+            if eventClusterId == last_eventClusterId: # TBD: this is flaky as we only check events adjacent in timeline
+                f.write('\t' + str(doc.doc_id) + '-' + str(nsentence)+ '-' + str_event) #+  "\t('" + str_timex + "')" 
+            else:
+                f.write('\n' + str(order) + '\t' + date_str + '\t' + str(doc_id) + '-' + str(nsentence)+ '-' + str_event) #+  "\t('" + str_timex + "')" 
+            last_eventClusterId = eventClusterId
+            last_stem = this_stem
+        f.close()
+    return timeline, corpus, collection
 
 def run(test_corpus_list = [1], train_corpus_list = [0], link_model='structured_perceptron'):
     ''' 
@@ -274,7 +275,10 @@ def run(test_corpus_list = [1], train_corpus_list = [0], link_model='structured_
         model_params.w = EventTIMEX3Linking.structuredPredictionTraining(collection_train_list, timex_syntactic_features)
 
     # Prediction
-    timelines_result = build_timeline(test_corpus_list, model_params)
+    timelines_result = []
+    for corpus in test_corpus_list:
+        (collection, targetEntityList) = getCollection(corpus)
+        timelines_result += [build_timeline(corpus, collection, targetEntityList, model_params)]
 
     # Evaluation
     for timeline, corpus, collection in timelines_result:
@@ -291,6 +295,62 @@ def run(test_corpus_list = [1], train_corpus_list = [0], link_model='structured_
             os.chdir('..')
         except:
             print 'Evaluation error'
+        
+def crossval(train_corpus = 0, link_model='structured_perceptron'):
+    ''' 
+    Perform cross-validation on the given corpus. Hold out one target entity while training on the rest.
+    Each run will generate one timeline file. At the end run the evaluation normally on the files created.
+    '''
+    print cdeo_config.getConfig('restrict_entity_linking_to_sentence_flag')
+
+    # Read in the training corpus
+    (collection_train_clean, targetEntityList_train) = getCollection(train_corpus)
+    goldTimelineDir = '../data/evaluation/corpus_%s/TimeLines_Gold_Standard' %(train_corpus) # TBD: clean this up
+
+    # remove all files in the results directory just to be sure
+    folder = '../data/evaluation/corpus_%s/results' %(train_corpus)
+    for the_file in os.listdir(folder):
+        file_path = os.path.join(folder, the_file)
+        try:
+            if os.path.isfile(file_path):
+                os.unlink(file_path)
+        except Exception, e:
+            print e
+    
+    for icross in range(len(targetEntityList_train)):
+        collection_train = copy.deepcopy(collection_train_clean)
+        trainEntityList = copy.deepcopy(targetEntityList_train)
+        testEntityList = [trainEntityList.pop(icross)]
+        
+        utils.annotateCollectionWithGoldTimeline(collection_train, trainEntityList, goldTimelineDir)
+        collection_train_list = [(collection_train, trainEntityList)]
+        
+        model_params = ModelParams(link_model)
+        model_params.entity_syntactic_features = EventEntityLinking.extractSyntacticFeatures(collection_train)
+        model_params.timex_syntactic_features = EventTIMEX3Linking.extractSyntacticFeatures(collection_train)
+
+        # Training
+        print '\n=============================================================='
+        print 'Training corpus %d, target entities %s' %(train_corpus, str(trainEntityList))
+        print '=============================================================='
+        if model_params.link_model == 'perceptron':
+            model_params.clfEntity = EventEntityLinking.trainEventEntityClassifier(collection_train_list, model_params.entity_syntactic_features)
+            model_params.clfTIMEX3 = EventTIMEX3Linking.trainEventTIMEX3Classifier(collection_train_list, model_params.timex_syntactic_features)
+
+        elif model_params.link_model == 'structured_perceptron':
+            model_params.w_ee = EventEntityLinking.structuredPredictionTraining(collection_train_list, model_params.entity_syntactic_features)
+            model_params.w = EventTIMEX3Linking.structuredPredictionTraining(collection_train_list, model_params.timex_syntactic_features)
+
+        # Prediction (this will save the timeline to a file.
+        timeline, corpus, collection  = build_timeline(train_corpus, collection_train, testEntityList, model_params)
+        
+    args = []
+    args += [("/home/sc/projects/cdeo/data/evaluation/corpus_%d/TimeLines_Gold_Standard/" %(corpus))]
+    args += [("/home/sc/projects/cdeo/data/evaluation/corpus_%d/results/" %(corpus))]
+    os.chdir('evaluation_tool')
+    fmicro, pmicro, rmicro = evaluation_all_function.run(args)
+    os.chdir('..')
+    return fmicro, pmicro, rmicro
 
 class ModelParams():
     def __init__(self, link_model):
